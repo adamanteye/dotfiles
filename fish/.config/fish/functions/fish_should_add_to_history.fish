e65afbb8976fbba26fddc69f9ebabe7a25bc4545
adamanteye <ada@adamanteye.cc>
2025-03-02 08:32:52 +0800

    feat: update

--- fish/.config/fish/functions/fish_should_add_to_history.fish
+++ fish/.config/fish/functions/fish_should_add_to_history.fish
@@ -1,5 +1,5 @@
 function fish_should_add_to_history
-    for cmd in cd git ls rm open
+    for cmd in cd git ls rm open cp
         string match -qr "^$cmd" -- $argv; and return 1
     end
     return 0
