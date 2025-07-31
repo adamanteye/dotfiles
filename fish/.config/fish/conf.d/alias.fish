07ca09c2b6cf818698955f0fd3bbb109a1105a2b
adamanteye <ada@adamanteye.cc>
2025-07-31 10:59:04 +0800

    feat(zellij): configure default layout
    Signed-off-by: adamanteye <ada@adamanteye.cc>

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -29,7 +29,7 @@ alias less='less -r'
 alias grep='grep --color=auto'
 alias tree='lsd --tree'
 alias cat='bat --style=plain --paging=never'
-alias htop='send-heartbeat "view htop" && /usr/bin/htop'
+alias htop='send-heartbeat "view htop" && glances'
 alias scp='gpg-connect-agent updatestartuptty /bye >/dev/null && /usr/bin/scp -C'
 alias rm='rm -vi'
 
@@ -46,8 +46,8 @@ alias weather-yinchuan='curl wttr.in/yinchuan'
 
 alias debian-builder='docker run -it --rm --name debian --network host -e HTTP_PROXY=http://[::1]:10801 -v "$HOME/Documents/debian:/home/debian:rw" -v "/etc/wgetrc:/etc/wgetrc:ro" -e DEBFULLNAME -e DEBEMAIL ghcr.io/adamanteye/debian-builder:latest'
 
-alias setproxy='export all_proxy=http://[::1]:10801'
-alias unsetproxy='export all_proxy='
+alias setproxy='export ALL_PROXY=http://[::1]:10801 && export HTTPS_PROXY=http://[::1]:10801'
+alias unsetproxy='export ALL_PROXY= && export HTTPS_PROXY='
 
 alias gitcountcode='git ls-files | grep -vE "(png|jpg|ico|svg|pdf|lock)" | xargs wc -l'
 alias gitcountall='git diff --stat "$(git hash-object -t tree /dev/null)"'
